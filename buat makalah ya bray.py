7eab65ba215cd47b906466487e835594330d93d1
Rizkarahma15 <rizkarahma1972@gmail.com>
2025-05-11 20:25:23 +0700

    Update buat makalah ya bray.py

--- buat makalah ya bray.py
+++ buat makalah ya bray.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image
+
 
 # Buat requirements.txt otomatis
 if not os.path.exists('requirements.txt'):
